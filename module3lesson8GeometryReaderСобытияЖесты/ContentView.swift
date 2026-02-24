de0286252627459617d972f2363c2c66d42eb5e1
Maksim Minakov <maksim.minakov.83@mail.ru>
2026-02-24 21:43:30 +0300

    learn onAppear

--- module3lesson8GeometryReaderСобытияЖесты/ContentView.swift
+++ module3lesson8GeometryReaderСобытияЖесты/ContentView.swift
@@ -1,23 +1,42 @@
-//
-//  ContentView.swift
-//  module3lesson8GeometryReaderСобытияЖесты
-//
-//  Created by Максим Минаков on 24.02.2026.
-//
-
 import SwiftUI
+import Combine
+
+class ContentViewModel: ObservableObject {
+    func getNews(){ print("1: onAppear") }
+} // <- old variant
 
 struct ContentView: View {
+    @StateObject var viewModel = ContentViewModel() // <- old variant
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {  // ← NavigationStack на верхнем уровне
+            VStack {
+                NavigationLink("Second Page", destination: SecondPage())
+                    .onAppear{
+                        print("2: onAppear")
+                    }
+            }
+            .padding()
+            .onAppear {
+                viewModel.getNews()
+            }
         }
-        .padding()
     }
 }
+
+struct SecondPage: View {
+    var body: some View {
+        VStack{
+            Text("Second Page")
+            
+        }
+        .onAppear{
+            print("3: onAppear")
+        }
+    }
+}
+
+
 
 #Preview {
     ContentView()
