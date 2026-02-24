89f4c2c192f7221f7c73914289caf93b45b4c2ef
Maksim Minakov <maksim.minakov.83@mail.ru>
2026-02-24 20:19:51 +0300

    Initial Commit

---
+++ module3lesson8GeometryReaderСобытияЖесты/module3lesson8GeometryReader____________App.swift
@@ -0,0 +1,17 @@
+//
+//  module3lesson8GeometryReader____________App.swift
+//  module3lesson8GeometryReaderСобытияЖесты
+//
+//  Created by Максим Минаков on 24.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct module3lesson8GeometryReader____________App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
